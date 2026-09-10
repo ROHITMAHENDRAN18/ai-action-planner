9374f83456d68b8919623975c60d67d231023ce9
Rithirajendran <rithirajendran006@gmail.com>
2026-09-10 21:53:15 +0530

    Add plan API endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,40 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class PlanRequest(BaseModel):
+    goal: str
 
 
 @app.get("/")
 def home():
     return {
         "message": "AI Action Planner Backend is running!"
+    }
+
+
+@app.post("/api/plan")
+def create_plan(request: PlanRequest):
+
+    return {
+        "goal": request.goal,
+        "tasks": [
+            {
+                "title": "Understand the topic",
+                "priority": "HIGH",
+                "hours": 2
+            },
+            {
+                "title": "Practice important questions",
+                "priority": "HIGH",
+                "hours": 2
+            },
+            {
+                "title": "Review what you learned",
+                "priority": "MEDIUM",
+                "hours": 1
+            }
+        ]
     }
